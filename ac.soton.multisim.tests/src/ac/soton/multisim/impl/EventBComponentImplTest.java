0ad6d8c620c13923dd3ac0be5e8976f25cafd46b
Vitaly <v.savicks@gmail.com>
2015-09-26 18:17:32 +0100

    Adds maven/tycho

---
+++ ac.soton.multisim.tests/src/ac/soton/multisim/impl/EventBComponentImplTest.java
@@ -0,0 +1,14 @@
+package ac.soton.multisim.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventBComponentImplTest {
+
+	@Test
+	public void testInstantiate() {
+		fail("Not yet implemented");
+	}
+
+}
